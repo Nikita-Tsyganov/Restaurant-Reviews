38ba8225810b8c9d6d19eed4cc9daf6617296a11
Acer Predator <nikita.s.tsyganov@gmail.com>
2018-03-16 23:52:34 +0200

    Adding original files.

---
+++ js/restaurant_info.js
@@ -0,0 +1,215 @@
+let restaurant;
+var map;
+
+/**
+ * Registering a Service Worker if supported.
+ */
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("./sw.js", {scope: './'})
+}
+
+/**
+ * Initialize Google map, called from HTML.
+ */
+window.initMap = () => {
+  fetchRestaurantFromURL((error, restaurant) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {
+      self.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: restaurant.latlng,
+        scrollwheel: false
+      });
+      //Accessibility feature
+      google.maps.event.addListener(self.map, "tilesloaded", function() {
+          const iframe = document.querySelector('#map iframe');
+          iframe.title = "Google Maps";
+      });
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+    }
+  });
+};
+
+/**
+ * Get current restaurant from page URL.
+ */
+fetchRestaurantFromURL = (callback) => {
+  if (self.restaurant) { // restaurant already fetched!
+    callback(null, self.restaurant);
+    return;
+  }
+  const id = getParameterByName('id');
+  if (!id) { // no id found in URL
+    error = 'No restaurant id in URL';
+    callback(error, null);
+  } else {
+    DBHelper.fetchRestaurantById(id, (error, restaurant) => {
+      self.restaurant = restaurant;
+      if (!restaurant) {
+        console.error(error);
+        return;
+      }
+      fillRestaurantHTML();
+
+      //Fill breadcrumb and upon finish apply Google Map avoiding focus feature.
+      fillBreadcrumb()
+          .then(
+            aElement => {
+              const footerLink = document.querySelector("footer a");
+              aElement.onkeydown = function (event) {
+                  if(!event.shiftKey && event.keyCode === 9) {
+                      event.preventDefault();
+                      footerLink.focus();
+                  }
+              };
+              footerLink.onkeydown = function (event) {
+                  if(event.shiftKey && event.keyCode === 9) {
+                      event.preventDefault();
+                      aElement.focus();
+                  }
+              };
+            },
+            errMessage => {
+                console.log(errMessage);
+            }
+          );
+      callback(null, restaurant)
+    });
+  }
+};
+
+/**
+ * Create restaurant HTML and add it to the webpage
+ */
+fillRestaurantHTML = (restaurant = self.restaurant) => {
+  const name = document.getElementById('restaurant-name');
+  name.innerHTML = restaurant.name;
+
+  const address = document.getElementById('restaurant-address');
+  address.innerHTML = restaurant.address;
+
+  const image = document.getElementById('restaurant-img');
+  image.className = 'restaurant-img';
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "Restaurant " + restaurant.name;
+
+  const cuisine = document.getElementById('restaurant-cuisine');
+  cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.setAttribute("aria-label", "The restaurant's cuisine is " + restaurant.cuisine_type);
+
+  // fill operating hours
+  if (restaurant.operating_hours) {
+    fillRestaurantHoursHTML();
+  }
+  // fill reviews
+  fillReviewsHTML();
+};
+
+/**
+ * Create restaurant operating hours HTML table and add it to the webpage.
+ */
+fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
+  const hours = document.getElementById('restaurant-hours');
+  for (let key in operatingHours) {
+    const row = document.createElement('tr');
+
+    const day = document.createElement('td');
+    day.innerHTML = key;
+    row.appendChild(day);
+
+    const time = document.createElement('td');
+    time.innerHTML = operatingHours[key];
+    row.appendChild(time);
+
+    hours.appendChild(row);
+  }
+};
+
+/**
+ * Create all reviews HTML and add them to the webpage.
+ */
+fillReviewsHTML = (reviews = self.restaurant.reviews) => {
+  const container = document.getElementById('reviews-container');
+  const title = document.createElement('h3');
+  title.innerHTML = 'Reviews';
+  container.appendChild(title);
+
+  if (!reviews) {
+    const noReviews = document.createElement('p');
+    noReviews.innerHTML = 'No reviews yet!';
+    container.appendChild(noReviews);
+    return;
+  }
+  const ul = document.getElementById('reviews-list');
+  reviews.forEach(review => {
+    ul.appendChild(createReviewHTML(review));
+  });
+  container.appendChild(ul);
+};
+
+/**
+ * Create review HTML and add it to the webpage.
+ */
+createReviewHTML = (review) => {
+  const li = document.createElement('li');
+
+  const header = document.createElement('h4');
+  header.classList.add("review-header");
+  li.appendChild(header);
+
+  const name = document.createElement('p');
+  name.innerHTML = review.name;
+  name.classList.add("review-name");
+  header.appendChild(name);
+
+  const date = document.createElement('p');
+  date.innerHTML = review.date;
+  date.classList.add("review-date");
+  header.appendChild(date);
+
+  const rating = document.createElement('p');
+  rating.innerHTML = `Rating: ${review.rating}`;
+  rating.classList.add("review-rating");
+  li.appendChild(rating);
+
+  const comments = document.createElement('p');
+  comments.innerHTML = review.comments;
+  comments.classList.add("review-comments");
+  li.appendChild(comments);
+
+  return li;
+};
+
+/**
+ *  Return a Promise adding restaurant name to the breadcrumb navigation menu
+ */
+fillBreadcrumb = (restaurant=self.restaurant) => {
+    return new Promise((resolve, reject) => {
+        const breadcrumb = document.getElementById('breadcrumb');
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = window.location.href;
+        a.setAttribute("aria-current", "page");
+        a.innerHTML =  restaurant.name;
+        li.appendChild(a);
+        breadcrumb.appendChild(li);
+        a.parentNode === li ? resolve(a) : reject("An error occurred while appending <a> to <li>");
+    }).then(aElement => aElement, error => {console.log(error);});
+};
+
+/**
+ * Get a parameter by name from page URL.
+ */
+getParameterByName = (name, url) => {
+  if (!url)
+    url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+    results = regex.exec(url);
+  if (!results)
+    return null;
+  if (!results[2])
+    return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
