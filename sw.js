8520be72f3250343e781ba5d6c70e6669d0f2703
Acer Predator <nikita.s.tsyganov@gmail.com>
2019-02-18 23:54:18 -0600

    Removed the 'install' event handler because it has never worked properly

--- sw.js
+++ sw.js
@@ -4,24 +4,6 @@ importScripts('./js/dbhelper.js');
 const staticCacheName = 'rr-static-v1';
 
 
-/*self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function(cache) {
-            return cache.addAll(
-                [
-                    '/index.html',
-                    '/restaurant.html',
-                    '/js/app-main-min.js',
-                    '/js/app-restaurant-min.js',
-                    '/css/main-min.css',
-                    '/css/restaurant_info-min.css',
-                    '/img/'
-                ]
-            );
-        })
-    );
-});*/
-
 self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
